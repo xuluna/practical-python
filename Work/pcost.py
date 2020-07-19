1c7c45bb03b2845c2330c97394afc4a2c36d4df0
Luna Xu <xuluna@ibm.com>
2020-07-19 18:13:50 -0400

    starting from 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import gzip
+cost = 0
+
+
+with gzip.open('Data/portfolio.csv.gz', 'rt') as file:
+    next(file)
+    for line in file:
+        shares = int(line.split(',')[1])
+        price = float(line.split(',')[2])
+        cost += shares*price
+
+print('Total cost', cost)
