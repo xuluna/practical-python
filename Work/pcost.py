ffcad47da45f5e7c3fff3a0980ce6b3af970edbe
Luna Xu <xuluna@ibm.com>
2020-08-16 22:05:05 -0400

    2.4

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,14 +3,31 @@
 # Exercise 1.27
 
 import gzip
-cost = 0
+import csv
+import sys
 
 
-with gzip.open('Data/portfolio.csv.gz', 'rt') as file:
-    next(file)
-    for line in file:
-        shares = int(line.split(',')[1])
-        price = float(line.split(',')[2])
-        cost += shares*price
+def portfolio_cost(filename):
+    cost = 0
 
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for line in rows:
+            try:
+                shares = int(line[1])
+                price = float(line[2])
+                cost += shares*price
+            except ValueError:
+                print("invalid row: ", line)
+
+    return cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,61 @@
 # report.py
 #
 # Exercise 2.4
+
+# pcost.py
+#
+# Exercise 1.27
+
+import csv
+from pprint import pprint
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for line in rows:
+            holding = {}
+            try:
+                holding['name'] = line[0]
+                holding['shares'] = int(line[1])
+                holding['price'] = float(line[2])
+                portfolio.append(holding)
+            except ValueError:
+                print("invalid row: ", line)
+
+    return portfolio
+
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        for row in rows:
+            if len(row) < 2:
+                continue
+            prices[row[0]] = float(row[1])
+    return prices
+
+
+def make_report(portfolio, prices):
+    report = []
+    for f in portfolio:
+        name = f['name']
+        share = f['shares']
+        price = prices[name]
+        change = price-f['price']
+        report.append((name, share, price, change))
+    return report
+
+
+prices = read_prices('Data/prices.csv')
+portfolio = read_portfolio('Data/portfolio.csv')
+report = make_report(portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' % headers)
+print(('-'*10+' ')*4)
+for name, share, price, change in report:
+    price = "${:.2f}".format(price)
+    print(f'{name:>10s} {share:>10d} {price:>10s} {change:>10.2f}')
